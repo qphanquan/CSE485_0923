9dcb478038007fc9d994303ab975aaf6b03af416
qphanquan <116238009+qphanquan@users.noreply.github.com>
2023-09-19 09:32:37 +0700

    up

---
+++ BTTH01A/BTTTH01_CSE485_ex01/Bai2.php
@@ -0,0 +1,6 @@
+<?php
+$arrs = ['đỏ', 'xanh', 'cam', 'trắng'];
+echo "Màu <span style='color:red'>". $arrs[0] ."</span> là màu yêu thích của Anh, <span style='color:green'>". $arrs[3] ."</span> là màu yêu thích của Sơn, <span style='color:orange'>". $arrs[2] ."</span> là màu yêu
+thích của Thắng, còn màu yêu thích của tôi là màu <span style='color:green'>". $arrs[3] ."</span>";
+
+
